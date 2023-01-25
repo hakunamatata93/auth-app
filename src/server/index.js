a911f9c4a4c6b511f7dca2d740270cc2673a47b9
hakunamatata93 <ivaschenkodavid@yandex.ru>
2023-01-25 16:58:30 +0400

    deploy

--- src/server/index.js
+++ src/server/index.js
@@ -2,20 +2,14 @@ const express = require('express');
 const authRouter = require('./routes/authRouter');
 const mainRouter = require('./routes/mainRouter');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
-const corsOptions = {
-    origin: "http://localhost:3000",
-    credentials: true,
-    optionsSuccessStatus: 200,
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH']
-}
-
 
 const app = express();
-
-app.use(cors(corsOptions));
+dotenv.config();
+app.use(cors);
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use('/main', mainRouter);
