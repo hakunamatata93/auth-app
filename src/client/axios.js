05c102625fd8b9f86656e9585c5fba7c2e10444f
hakunamatata93 <ivaschenkodavid@yandex.ru>
2023-01-25 17:08:16 +0400

    Update axios.js

--- src/client/axios.js
+++ src/client/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+
+const APP_URL = process.env.APP_URL || 'http://localhost:5000'
 const instance = axios.create({
-  baseURL: "",
+  baseURL: APP_URL,
 });
 
 export default instance;
